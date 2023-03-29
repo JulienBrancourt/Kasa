6c40a6d238db1e3f5fcdc14e79de46b6489b63aa
ElyanisJ <brancourt.julien@hotmail.fr>
2023-03-29 12:59:28 +0200

    maj banner header et compteur slideshow

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,9 +1,22 @@
+import { useLocation } from 'react-router-dom';
+import { useState,useEffect } from 'react';
 import styles from './Banner.module.css'
+
 
 //on passe la src de l'image et la présence du titre en props pour pouvoir personnaliser Banner en fonction de la page
 function Banner(props) {
+    const [homePage, setHomePage] = useState(false)
+    const location = useLocation()
+
+    useEffect(() => {
+		if(location.pathname === '/'){
+			setHomePage(true)
+		};
+
+	},[location.pathname])
+
     return (
-        <div className={styles.container}>
+        <div className={homePage ? styles.containerHome : styles.containerApropos}>
             <img src={props.sourceImage} alt={props.texteAltImage} className={styles.imageBanner} />
             <div className={styles.filtre}></div>
 {/* condition à mettre entre accolades : si on a props.Titre alors on affiche h1 */}
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,6 @@
-import { Link } from 'react-router-dom'
+// import { useLocation } from 'react-router-dom'
+// import { useState, useEffect } from 'react'
+import { NavLink } from 'react-router-dom'
 import logo from '../../assets/LogoHeader.png'
 import styles from './Header.module.css'
 
@@ -7,8 +9,20 @@ function Header() {
         <header className={styles.header}>
             <img src={logo} alt='Logo Kasa du header' className={styles.logo}/>
             <nav className={styles.nav}>
-                <Link to="/" className={styles.accueil}>Accueil</Link>
-                <Link to="/a-propos" className={styles.about}>A Propos</Link>
+                <NavLink 
+                    exact to="/" 
+                    className={({isActive}) => (
+                        isActive ? styles.accueilsouligne : styles.accueil)}
+                    strict="true">
+                    Accueil
+                </NavLink>
+                <NavLink 
+                    exact to="/a-propos/" 
+                    className={({isActive}) => (
+                        isActive ? styles.aboutsouligne : styles.about)} 
+                    strict="true">
+                    A Propos
+                </NavLink>
             </nav>
         </header>
         
